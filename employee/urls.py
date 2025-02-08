4f8c5938e0b7f62ac503804a0aeb816bf98318d1
tohidrupok <tohidrupok@gmail.com>
2025-02-08 13:59:21 +0600

    add company profile for employee

--- employee/urls.py
+++ employee/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path('profile/', views.view_employer_profile, name='employer_profile'), 
+    
     path('dashboard/', views.employer_dashboard, name='employer_dashboard'),
-    path('profile/', views.employer_profile, name='employer_profile'),
-    path('profile/edit', views.edit_employer_profile, name='employer_profile_edit'),
-    path('core/profile/edit/', views.employer_profile, name='core_employer_profile_edit'),
+    path('core/profile/edit/', views.employer_profile, name='employer_profile_edit'),
     
 ]
 
--- employee/views.py
+++ employee/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseForbidden
 from accounts.models import EmployerProfile
 from .forms import EmployerProfileForm
 from django.http import JsonResponse, HttpResponseForbidden
+from django.utils import timezone
 
 
 # Create your views here.    
@@ -19,39 +20,47 @@ def employer_dashboard(request):
 
 
 @login_required
-def employer_profile(request):
+def view_employer_profile(request):
     if not request.user.is_employer:  
         return HttpResponseForbidden("Access restricted to employers.")
     if not request.user.is_approved:
         return render(request, 'registration/pending_approval.html') 
     
     profile = get_object_or_404(EmployerProfile, user=request.user)
-    return render(request, 'employer-detail-v2.html', {'profile': profile})   
+    
+    total_experience = None
+    if profile.founded_date:
+        total_experience = timezone.now().year - profile.founded_date.year
+        
+    return render(request, 'employer-detail-v2.html', {
+        'profile': profile,
+        'total_experience': total_experience,
+    })   
  
 
 
-@login_required
-def edit_employer_profile(request):
-    if not request.user.is_employer:
-        return HttpResponseForbidden("Access restricted to employers.")
+# @login_required
+# def edit_employer_profile(request):
+#     if not request.user.is_employer:
+#         return HttpResponseForbidden("Access restricted to employers.")
     
     
-    profile = get_object_or_404(EmployerProfile, user=request.user)
+#     profile = get_object_or_404(EmployerProfile, user=request.user)
     
-    if request.method == "POST" and request.FILES.get("logo"):
-        profile.logo = request.FILES["logo"]
-        profile.save()
-        return JsonResponse({"success": True, "logo_url": profile.logo.url}) 
+#     if request.method == "POST" and request.FILES.get("logo"):
+#         profile.logo = request.FILES["logo"]
+#         profile.save()
+#         return JsonResponse({"success": True, "logo_url": profile.logo.url}) 
     
-    if request.method == "POST":
-        form = EmployerProfileForm(request.POST, request.FILES, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect('employer_profile')  # Redirect to the profile page after saving
-    else:
-        form = EmployerProfileForm(instance=profile)
+#     if request.method == "POST":
+#         form = EmployerProfileForm(request.POST, request.FILES, instance=profile)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('employer_profile')  # Redirect to the profile page after saving
+#     else:
+#         form = EmployerProfileForm(instance=profile)
     
-    return render(request, 'employer-profile.html', {'form': form, 'profile': profile}) 
+#     return render(request, 'employer-profile.html', {'form': form, 'profile': profile}) 
 
 
 @login_required
