cb2d58071bdc2515283bffa061c79e582bf6d6c8
tohidrupok <tohidrupok@gmail.com>
2025-03-16 12:30:22 +0600

    deshboar profile delete added

--- AdminPro/urls.py
+++ AdminPro/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import superuser_dashboard
+from .views import superuser_dashboard, delete_profile
 
 urlpatterns = [
     path('dashboard/', superuser_dashboard, name='superuser_dashboard'),
+    path('delete-profile/<int:user_id>/', delete_profile, name='delete_profile'),
+    
 ]
--- AdminPro/views.py
+++ AdminPro/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from jobboard.models import JobApplication 
 from jobboard.models import JobPost
 from django.utils import timezone
+from accounts.models import CustomUser
+from django.contrib import messages
 
 # Function to check if user is superuser & staff
 def is_superuser(user):
@@ -30,3 +32,17 @@ def superuser_dashboard(request):
         'recent_job_applications':recent_job_applications
     })
 
+@login_required
+@user_passes_test(is_superuser)  # Restrict to superusers only
+def delete_profile(request, user_id):
+    
+    user = get_object_or_404(CustomUser, id=user_id) 
+    print(user)
+    if user.is_superuser:
+        messages.error(request, "Superuser accounts cannot be deleted.")
+        return redirect('superuser_dashboard')
+
+    user.delete()
+    print("Delet hoye gacha")
+    messages.success(request, f"User {user.username} has been deleted successfully.")
+    return redirect('superuser_dashboard')  # Redirect after deletion
